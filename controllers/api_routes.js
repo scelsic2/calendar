9c92bb532ed0a0c43fbe13de239cd570f46f3937
scelsic2 <scelsic2@gmail.com>
2023-09-22 11:28:05 -0400

    routing date problem

--- controllers/api_routes.js
+++ controllers/api_routes.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
 const dayjs = require('dayjs')
 
-router.get('/', (req, res) => {
-    const now = dayjs().format('MMMM');
-    res.render('index', {
-        now: now,
-    });
-});
+router.get('/', async (req, res) => {
+    res.render('index')
+})
+
+// router.get('/day', async (req, res) => {
+//     console.log(req.params)
+//     let date = 1
+//     res.render('day', {date: date})
+// });
 
 module.exports = router;
